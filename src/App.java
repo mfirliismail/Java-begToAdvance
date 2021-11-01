41d6927183f3da15fc0ec756dc8b2726b1bcd6ed
mfirliismail <m.firliismail@gmail.com>
2021-11-01 18:13:47 +0700

    save

---
+++ src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int aNumber;
+        aNumber = 123;
+
+        short aShort = 93;
+        long aLong = 1312;
+
+        double aDouble = 3.14;
+        float aFloat = 5.323f;
+
+        char aChar = 'a';
+
+        boolean aBoolean = true;
+        byte aByte = 123;
+
+        int hasil;
+        hasil = aNumber + aShort;
+        System.out.println(aShort);
+        System.out.println(aNumber);
+        System.out.println(hasil);
+        System.out.println(aLong);
+        System.out.println(aDouble);
+        System.out.println(aChar);
+        System.out.println(aFloat);
+        System.out.println(aByte);
+        System.out.println(aBoolean);
+    }
+
+}
